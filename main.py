1b1c29e87ca0c61f31da111ac407121089023cc0
adi gollan <ddd@gm.com>
2025-01-31 11:38:34 +0200

    version 1.1

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.
-
-
 
 a = np.array([1, 2, 3])
 
 b = np.array([50, 534, 2])
 
-
+c = a + b
 print(a * b)
+print(c)
