e1b4ac7f90bbedc5032085392d2f7a883434cbb7
adi gollan <ddd@gm.com>
2025-02-01 12:48:59 +0200

    added pygame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pygame 
 
 a = np.array([1, 2, 3])
 
@@ -12,3 +13,12 @@ print(c)
 
 print("another fucking version")
 print("GG")
+
+window = pygame.display.set_mode((400, 400))
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    window.fill((255, 55 ,255))
+    pygame.display.flip()
